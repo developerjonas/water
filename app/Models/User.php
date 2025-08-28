259c76f725f7567c97d3dbbba2f22392679ab5af
developerjonas <thedeveloperjonas@gmail.com>
2025-08-28 21:19:28 +0545

    Model Fix

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,10 +5,20 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasFactory, Notifiable;
+
+    /**
+     * Allow any user to access the Filament admin panel (for demo purposes)
+     */
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
+    }
 
     /**
      * The attributes that are mass assignable.
